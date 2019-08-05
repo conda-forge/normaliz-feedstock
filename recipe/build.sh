89e7479fe438bd3800529c23e7e6ae8407139fd0
Isuru Fernando <isuruf@gmail.com>
2019-08-05 05:46:22 +0000

    Use nauty and e-antic

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --with-e-antic=$PREFIX --with-nauty=$PREFIX --with-flint=$PREFIX --with-gmp=$PREFIX
 make -j${CPU_COUNT}
 make check
 make install
