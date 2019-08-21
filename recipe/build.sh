0e8c0e9f51486a64599c313eaa5cb7e0f6980474
Isuru Fernando <isuruf@gmail.com>
2019-08-21 14:42:59 -0500

    remove cl_wrapper and force use of lld

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,15 +4,13 @@ set -e
 
 autoreconf --install
 chmod +x configure
-which make
-make --version
 
-case `uname` in
-    Darwin|Linux)
+case "$target_platform" in
+    linux*|osx*)
         export CFLAGS="-O3 -g -fPIC $CFLAGS"
         ./configure --prefix=$PREFIX --with-e-antic=$PREFIX --with-nauty=$PREFIX --with-flint=$PREFIX --with-gmp=$PREFIX
         ;;
-    MINGW*)
+    win*)
         export PATH="$PREFIX/Library/bin:$BUILD_PREFIX/Library/bin:$RECIPE_DIR:$PATH"
         export CC=clang.exe
         export CXX=clang++.exe
@@ -21,17 +19,19 @@ case `uname` in
         export AR=llvm-ar
         export LD=lld-link
         export CCCL=clang-cl
-        export CFLAGS="-MD -I$PREFIX/Library/include -O2 -D_CRT_SECURE_NO_WARNINGS"
-        export CXXFLAGS="-MD -I$PREFIX/Library/include -O2 -D_CRT_SECURE_NO_WARNINGS"
-        export LDFLAGS="$LDFLAGS -L$PREFIX/Library/lib"
+        export CFLAGS="-I$PREFIX/Library/include -O2 -D_CRT_SECURE_NO_WARNINGS"
+        export CXXFLAGS="-I$PREFIX/Library/include -O2 -D_CRT_SECURE_NO_WARNINGS"
+        export LDFLAGS="$LDFLAGS -L$PREFIX/Library/lib -fuse-ld=lld"
         export lt_cv_deplibs_check_method=pass_all
-        export LIBS="-llibomp"
         cp $PREFIX/Library/lib/gmp.lib $PREFIX/Library/lib/gmpxx.lib
         ./configure --prefix="$PREFIX/Library" --with-nauty=$PREFIX -with-gmp="$PREFIX/Library" || (cat config.log; false)
-        # libtool has support for using either nm or dumpbin, but neither works correctly with C++ mangling schemes
+
+        # libtool has support for using either nm or dumpbin, but neither works correctly with C++ mangling schemes.
+        # libtool also uses /EXPORT directives which only clang-cl understands.
         # cmake's dll creation tool works, but need to hack libtool to get it working
-        sed -i.bak "s/export_symbols_cmds=/export_symbols_cmds2=/g" libtool
-        sed "s/archive_expsym_cmds=/archive_expsym_cmds2=/g" libtool > libtool2
+        sed -i.bak 's/export_symbols_cmds=/export_symbols_cmds2=/g' libtool
+        sed -i.bak 's/|-fuse-linker-plugin|/|-fuse-linker-plugin|-fuse-ld=*|/g' libtool
+        sed 's/archive_expsym_cmds=/archive_expsym_cmds2=/g' libtool > libtool2
         echo "#!/bin/bash" > libtool
         echo "export_symbols_cmds=\"echo \\\$libobjs | \\\$SED 's/ /\n/g'  > \\\$export_symbols.tmp && cmake -E __create_def \\\$export_symbols \\\$export_symbols.tmp\"" >> libtool
         echo "archive_expsym_cmds=\"\\\$CC -o \\\$tool_output_objdir\\\$soname \\\$libobjs \\\$compiler_flags \\\$deplibs -Wl,-DEF:\\\\\\\"\\\$export_symbols\\\\\\\" -Wl,-DLL,-IMPLIB:\\\\\\\"\\\$tool_output_objdir\\\$libname.dll.lib\\\\\\\"; echo \"" >> libtool
@@ -40,10 +40,10 @@ case `uname` in
 esac
 
 make -j${CPU_COUNT}
-make check -k || (ls -alR test/run_tests && exit 1)
+make check -j${CPU_COUNT}
 make install
 
-if [[ `uname` == MINGW* ]]; then
+if [[ "$target_platform" == win* ]]; then
     PROJECT=normaliz
     LIBRARY_LIB=$PREFIX/Library/lib
     mv "${LIBRARY_LIB}/${PROJECT}.lib" "${LIBRARY_LIB}/${PROJECT}_static.lib"
