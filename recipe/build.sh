d23a4b1a0bdf3a4c09c3840080cdaf8fc2751593
Isuru Fernando <isuruf@gmail.com>
2020-07-18 14:25:24 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,6 +14,8 @@ case "$target_platform" in
     win*)
         cp $PREFIX/lib/gmp.lib $PREFIX/lib/gmpxx.lib
         sed -i.bak "s/-Wl,-rpath,/-L/g" configure
+        # https://github.com/Normaliz/Normaliz/pull/353/files
+        echo -e "#include <ctime>\n$(cat source/maxsimplex/maxsimplex.cpp)" > source/maxsimplex/maxsimplex.cpp
         ./configure --prefix="$PREFIX" --with-nauty=$PREFIX --with-gmp="$PREFIX" || (cat config.log; false)
         patch_libtool
         echo $?
