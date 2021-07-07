2850366abca9eb4e0399ecbf1d569ab716e62104
Isuru Fernando <isuruf@gmail.com>
2021-07-07 01:56:14 -0500

    Fix flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -20,7 +20,8 @@ case "$target_platform" in
         ;;
     win*)
         cp $PREFIX/lib/gmp.lib $PREFIX/lib/gmpxx.lib
-        cp $PREFIX/include/omp.h $BUILD_PREFIX/Library/lib/clang/*/include/
+        export CFLAGS="$CFLAGS -I${LIBRARY_INC}"
+        export LDFLAGS="$LDFLAGS -L${LIBRARY_LIB}"
         sed -i.bak "s/-Wl,-rpath,/-L/g" configure
         # https://github.com/Normaliz/Normaliz/pull/353/files
         echo -e "#include <ctime>\n$(cat source/maxsimplex/maxsimplex.cpp)" > source/maxsimplex/maxsimplex.cpp
