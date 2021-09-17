b7a552b9ad13f7613eca2d75580e94a3cae3c1ed
Isuru Fernando <isuruf@gmail.com>
2021-09-17 14:05:55 -0500

    run tests only on linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,8 +16,7 @@ case "$target_platform" in
     win*)
         cp $PREFIX/lib/gmp.lib $PREFIX/lib/gmpxx.lib
         sed -i.bak "s/-Wl,-rpath,/-L/g" configure
-        # https://github.com/Normaliz/Normaliz/pull/353/files
-        echo -e "#include <ctime>\n$(cat source/maxsimplex/maxsimplex.cpp)" > source/maxsimplex/maxsimplex.cpp
+		sed -i.bak "s@#include <sys/time.h>@@g" source/libnormaliz/full_cone.h
         ./configure --prefix="$PREFIX" --with-nauty=$PREFIX --with-gmp="$PREFIX" || (cat config.log; false)
         patch_libtool
         echo $?
@@ -26,10 +25,12 @@ esac
 
 make -j${CPU_COUNT}
 echo $?
-if [[ "$PKG_VERSION" == "3.8.5" ]]; then
-  make check -j${CPU_COUNT} -k || true;
-elif [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-  make check -j${CPU_COUNT} -k
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
+  if [[ "$target_platform" = linux-* ]]; then
+    make check -j${CPU_COUNT} -k
+  else
+    make check -j${CPU_COUNT} -k || true;
+  fi
 fi
 make install
 echo $?
