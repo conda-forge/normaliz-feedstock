9e6de18965d3d1563d7521c257c546b8de5b41e8
Julian Rüth <julian.rueth@fsfe.org>
2022-01-17 21:54:00 -0600

    Fix windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
 set "REMOVE_LIB_PREFIX=no"
 del "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.16.27023\include\omp.h"
 call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
-if errorlevel 1 exit 1
+if %errorlevel% neq 0 exit 1
