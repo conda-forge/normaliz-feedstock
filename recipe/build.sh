d605c2ca45f31c3848c64991cec1b8b8b2d0fb11
Isuru Fernando <isuruf@gmail.com>
2022-09-01 20:47:40 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,7 +17,7 @@ case "$target_platform" in
         cp $PREFIX/lib/gmp.lib $PREFIX/lib/gmpxx.lib
         sed -i.bak "s/-Wl,-rpath,/-L/g" configure
         sed -i.bak "s@#include <sys/time.h>@@g" source/libnormaliz/full_cone.h
-        export CFLAGS="-I$LIBRARY_PREFIX\include $CFLAGS"
+        export CFLAGS="-v $CFLAGS"
         ./configure --prefix="$PREFIX" --with-nauty=$PREFIX --with-gmp="$PREFIX" || (cat config.log; false)
         patch_libtool
         echo $?
